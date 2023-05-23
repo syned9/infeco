870a01f4dcaaae2021b44e3a8b7f32c0d381eb45
Denys <denysn9@gmail.com>
2023-05-23 22:10:45 +0200

    mise en forme page login

--- app/forms.py
+++ app/forms.py
@@ -57,8 +57,8 @@ class UserForm(FlaskForm):
     submit = SubmitField('Créer')
 
 class LoginForm(FlaskForm):
-    username = StringField('', validators=[DataRequired()], render_kw={"placeholder": "Nom d'utilisateur"})
-    password = PasswordField('', validators=[DataRequired()], render_kw={"placeholder": "Mot de passe"})
+    username = StringField('Nom d\'utilisateur', validators=[DataRequired()]) 
+    password = PasswordField('Mot de passe', validators=[DataRequired()])
     # remember_me = BooleanField('Se souvenir de moi')
     submit = SubmitField('Connexion')
 
