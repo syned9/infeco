80a8bd5c67db3a215b6b902119cab83d990d68e7
Denys <denysn9@gmail.com>
2023-05-23 17:08:23 +0200

    ajout dossier instance pour deploiement

---
+++ instance/config.py
@@ -0,0 +1,24 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'developpement POC infeco'
+    TEMPLATE_FOLDER = 'templates'
+    SECURITY_PASSWORD_SALT = 'infecoblock3'
+
+class ProductionConfig(Config):
+    DATABASE_URI = os.environ['DATABASE_URL']
+    # DATABASE_URI = os.environ['JAWSDB_URL']
+    DEBUG = False
+    FLASK_ENV = 'production'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TEMPLATES_AUTO_RELOAD = True
+    FLASK_ENV = 'development'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://infeco:kyzSR9aj6ZGHVm@127.0.0.1:3306/infeco'
+
+class TestingConfig(Config):
+    TESTING = True
